a14f5375c29a2e1312f056e8d583003df4fd78c0
Niklavs Laicans <niklavs.laicans@gmail.com>
2025-08-05 15:59:20 +0300

    Text marquee created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,8 @@ import Projects from "../components/sections/Projects";
 import Experience from "../components/sections/Experience";
 import Footer from "../components/common/Footer";
 import Expertise from "../components/sections/Expertise";
+import TextMarquee from "../components/TextMarquee";
+import SkillsMarquee from "../components/SkillsMarquee";
 
 const Home: FC = () => {
   return (
@@ -13,6 +15,7 @@ const Home: FC = () => {
       <Expertise />
       <Projects />
       <Experience />
+      <TextMarquee />
       <Footer />
     </Layout>
   );
